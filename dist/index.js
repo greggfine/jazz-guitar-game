a3e9dd8d03c8970fa0721645cc0bd485e180bda7
Gregg Fine <bbeez8500@gmail.com>
2019-11-18 17:57:16 -0500

    updated dist folder before deploy

--- dist/index.js
+++ dist/index.js
@@ -1,52 +1,53 @@
 "use strict";
 
-// window.onload = () => {
-    const score = document.getElementById('score');
-    const lives = document.getElementById('lives');
-    const startGameBtn = document.getElementById('start-game-btn');
-    const startGameDisplay = document.getElementById('start-game-display');
-    const questionAndAnswerDisplay = document.getElementById('question-answer-display');
-    const scoreWrapper = document.getElementById('score-wrapper');
-    const livesWrapper = document.getElementById('lives-wrapper');
-    const volumeControl = document.getElementById('volume-control');
-    const questionCount = document.getElementById('question-count');
-    const whoIsThis = document.getElementById('who-is-this');
-    const playGame = document.getElementById('play-game');
-    const btnGroup = playGame.querySelector('.btn-group');
-    const gameOverDisplay = document.getElementById('game-over');
-    const audio = document.createElement('audio');
-    const successSFX = document.createElement('audio');
-    const failureSFX = document.createElement('audio');
-    const gameOverSFX = document.createElement('audio');
-    const img = document.createElement('img');
-    const container = document.querySelector('.container');
+(function() {
+    const score = document.getElementById('score'),
+          lives = document.getElementById('lives'),
+          startGameBtn = document.getElementById('start-game-btn'),
+          startGameDisplay = document.getElementById('start-game-display'),
+          questionAndAnswerDisplay = document.getElementById('question-answer-display'),
+          scoreWrapper = document.getElementById('score-wrapper'),
+          livesWrapper = document.getElementById('lives-wrapper'),
+          volumeControl = document.getElementById('volume-control'),
+          questionCount = document.getElementById('question-count'),
+          whoIsThis = document.getElementById('who-is-this'),
+          playGame = document.getElementById('play-game'),
+          btnGroup = playGame.querySelector('.btn-group'),
+          gameOverDisplay = document.getElementById('game-over'),
+          audio = document.createElement('audio'),
+          successSFX = document.createElement('audio'),
+          failureSFX = document.createElement('audio'),
+          gameOverSFX = document.createElement('audio'),
+          img = document.createElement('img'),
+          container = document.querySelector('.container');
 
-    let scoreCount = 0;
-    let questionCountCounter = 1;
-    let livesCount = 3;
-    let currentTrack;
+    let scoreCount = 0,
+        questionCountCounter = 1,
+        livesCount = 3,
+        currentTrack;
+
+    successSFX.src = './audio/sfx/success/gregg-omnisphere5.mp3';
+    failureSFX.src = './audio/sfx/failure/2_DANCE_FX_DANCE_1_003.mp3';
+    gameOverSFX.src = './audio/sfx/game_over/Cutting Power.mp3';
     
     score.textContent = scoreCount;
     questionCount.textContent = `${questionCountCounter}/10`;
     lives.textContent = livesCount;
-    successSFX.src = './audio/sfx/success/gregg-omnisphere5.mp3';
-    // failureSFX.src = './audio/sfx/failure/Soft.wav';
-    failureSFX.src = './audio/sfx/failure/2_DANCE_FX_DANCE_1_003.mp3';
-    gameOverSFX.src = './audio/sfx/game_over/Cutting Power.mp3';
+
     
     volumeControl.addEventListener('click', function () {
-        audio.muted? audio.muted = false : audio.muted = true
-        successSFX.muted? successSFX.muted = false : successSFX.muted = true
-        failureSFX.muted? failureSFX.muted = false : failureSFX.muted = true
+        audio.muted? audio.muted = false : audio.muted = true;
+        successSFX.muted? successSFX.muted = false : successSFX.muted = true;
+        failureSFX.muted? failureSFX.muted = false : failureSFX.muted = true;
         const iconElement = this.querySelector('i');
         iconElement.classList.toggle('fa-volume-up');
         iconElement.classList.toggle('fa-volume-off');
     })
 
-    const guitaristNames = tracks.map(track => track.name);
-    const setupButtons = () => randomBtnAppend(createCorrectButton(currentTrack), createWrongButton());
-    const getRandomTrack = () => tracks[Math.floor(Math.random() * tracks.length)];
-    const getRandomName = (arr) => arr[Math.floor(Math.random() * guitaristNames.length)];
+    const guitaristNames = tracks.map(track => track.name),
+          setupButtons = () => randomBtnAppend(createCorrectButton(currentTrack), createWrongButton()),
+          getRandomTrack = () => tracks[Math.floor(Math.random() * tracks.length)],
+          getRandomName = (arr) => arr[Math.floor(Math.random() * guitaristNames.length)];
 
     function init() {
         gameOverSFX.muted = true; 
@@ -94,7 +95,7 @@
 
     function displayName(){
         const h3 = document.createElement('h3');
-        h3.style.fontSize = '2rem';
+        h3.classList.add('name-display');
         h3.textContent = currentTrack.name;
         questionAndAnswerDisplay.appendChild(h3);
     }
@@ -139,8 +140,9 @@
             gameOverSFX.muted = false;
             gameOverSFX.play();
             img.style.display = playGame.style.display = 'none';
-            gameOverDisplay.innerHTML = ` <h1>Game Over<h1>
+            gameOverDisplay.innerHTML = `<h1>Game Over<h1>
                  <h2>Final Score: ${scoreCount}<h2>`
+                 gameOverDisplay.classList.add('game-over-display')
             createPlayAgainButton();
         }, 2000)
     }
@@ -156,9 +158,7 @@
                 <i class="fa fa-play" aria-hidden="true">
                 </i>
                 <span>Play Again?</span>`;
-        button.classList.add('start-game-btn');
-        button.style.padding = '3rem 1.5rem';
-        button.querySelector('i').style.fontSize = '4rem';
+        button.classList.add('play-again-btn');
         gameOverDisplay.appendChild(button);
         button.addEventListener('click', () => init());
     }
@@ -210,5 +210,4 @@
     }
 
     startGame()
- 
-// }
+ })();
